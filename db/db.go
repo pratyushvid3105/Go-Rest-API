c1ed3854d8726c084aedf67fab1d0d0061265b79
pratyush3105 <pratyush.vidyarthi@amagi.com>
2024-10-29 18:43:19 +0530

    creating a sql db table

--- db/db.go
+++ db/db.go
@@ -6,9 +6,12 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var DB *sql.DB
+
 func InitDB(){
+	var err error
 	// api.db file will be created if not present. Technically when calling Open, we're not really creating a connection. Instead, we're just opening the database, and Go is then able to open connections thereafter.
-	DB, err := sql.Open("sqlite3", "api.db")
+	DB, err = sql.Open("sqlite3", "api.db")
 	if err != nil {
 		panic("Could not connect to DB")
 	}
@@ -18,4 +21,25 @@ func InitDB(){
 
 	// In addition to setting the max number, we can also set the max idle connection number, which simply means how many connections we want to keep open if no one's using these connections at the moment. And with that, I'm making sure that we have at least five connections open at all time, so that if we have an incoming request, it can immediately be handled, but more connections will be opened all the way up to a number of 10 connections if we have more requests coming in and if we need more connections. If we need more than 10, those other requests will have to wait until a connection is available again.
 	DB.SetMaxIdleConns(5)
+
+	createTables()
+}
+
+func createTables(){
+	createEventsTable := `
+	CREATE TABLE IF NOT EXISTS events (
+	    id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		description TEXT NOT NULL,
+		location TEXT NOT NULL,
+		dateTime DATETIME NOT NULL,
+		userId INTEGER
+	)
+	`
+
+	_, err := DB.Exec(createEventsTable)
+
+	if err != nil {
+		panic("Could not create events table.")
+	}
 }
--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pratyushvid3105/Go-Rest-API/db"
 	"github.com/pratyushvid3105/Go-Rest-API/models"
 )
 
 func main(){
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents) // GET, POST, PUT, PATCH, DELETE
@@ -30,9 +32,6 @@ func createEvent(context *gin.Context){
 		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse data"})
 		return
 	}
-
-	// event.ID = 1
-	// event.UserID = 1
 
 	event.Save()
 
