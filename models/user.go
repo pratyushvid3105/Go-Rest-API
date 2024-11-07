e88f04800706335c016a86a4d56532f0c2ca0d50
pratyush3105 <pratyush.vidyarthi@amagi.com>
2024-11-07 15:31:24 +0530

    added comment (dont store plain text passwords)

--- models/user.go
+++ models/user.go
@@ -9,6 +9,7 @@ type User struct{
 }
 
 func (u User) Save() error{
+	// the password that is stored in the database should not be the plain password the user entered. Instead, it should be hashed, so converted to a different value in a way that can't be reversed, so that we can't get the original password from the hashed value
 	query := "INSERT INTO users(email, password) VALUES(?, ?)"
 
 	result, err := db.DB.Exec(query, u.Email, u.Password)
