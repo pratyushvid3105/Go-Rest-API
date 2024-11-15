8acffcd4b2825b542f88e8f8286723dd602dcdce
pratyush3105 <pratyush.vidyarthi@amagi.com>
2024-11-15 19:28:43 +0530

    enhancing & using the middleware

--- routes/routes.go
+++ routes/routes.go
@@ -9,13 +9,19 @@ import (
 func RegisterRoutes(server *gin.Engine){
 	server.GET("/events/:id", getEvent)
 	server.GET("/events", getEvents) // GET, POST, PUT, PATCH, DELETE
+	server.GET("/users", getUsers)
 
-	// The first argument after the path, before createEvent, we can use the middlewares package And then refer to that Authenticate function like this because with Gin, we can register multiple request handlers and they will be executed from left to right.
-	server.POST("/events", middlewares.Authenticate, createEvent)
-	server.PUT("/events/:id", updateEvent)
-	server.DELETE("/events/:id", deleteEvent)
+	// We'll use authentication on multiple routes, also on PUT and DELETE, for example, there is an easier approach we can use. We can use that server here and call the Group method to create a group of routes. Now, Group then wants a path with which all grouped routes will start and here we'll just use "/" because that's the one thing all grouped routes will have in common. Then as a result, we'll get back a router group, which we should store in a variable (here "authenticated"). And now we can use that router group here to add routes to it.
+	authenticated := server.Group("/")
+
+	// To protect the routes, we should also use that Group to set up a middleware that will always be executed, and we do that with the "Use" method.
+	authenticated.Use(middlewares.Authenticate)
+
+	authenticated.POST("/events", createEvent)
+	authenticated.PUT("/events/:id", updateEvent)
+	authenticated.DELETE("/events/:id", deleteEvent)
+
 	server.POST("/signup", signup)
 	server.POST("/login", login)
-	server.GET("/users", getUsers)
 	// So that's how we now register routes in this function. And since we're always operating on exactly the same server value, since we're using a pointer here, we don't have to return anything here or do anything like that. Instead we are manipulating the original server when this function (RegisterRoutes) here is executed.
 }
